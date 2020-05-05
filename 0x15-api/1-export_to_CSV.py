94cd5ff7fa54c92634e445cab6bb67890b3b2f03
paurbano <paurbano@gmail.com>
2020-05-05 02:36:18 +0000

    request an API and resylt Export to CSV

---
+++ 0x15-api/1-export_to_CSV.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+#  sends a request to the URL and displays the value of
+# the variable X-Request-Id in the response header
+""" script that fetches https://intranet.hbtn.io/status """
+
+import csv
+import requests
+import sys
+
+if __name__ == "__main__":
+    url = 'https://jsonplaceholder.typicode.com/users/'
+
+    reqEmployee = requests.get(url + sys.argv[1])
+    reqTodos = requests.get(url + sys.argv[1] + '/todos/')
+    jsonemp = reqEmployee.json()
+    filename = jsonemp.get('id')
+    with open(filename, 'w') as csvfile:
+        csvwriter = csv.writer(csvfile, delimiter=",",
+                               quotechar='"', quoting=csv.QUOTE_ALL)
+        for task in reqTodos.json():
+            csvwriter = writerow(task.get('userId'), jsonemp.get('username'),
+                                 task.get('completed'), task.get('title'))
