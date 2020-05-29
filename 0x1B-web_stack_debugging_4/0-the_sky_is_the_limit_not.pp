154db27a9c7f41f3b305cebc204a9bcb3ee71dad
paurbano <paurbano@gmail.com>
2020-05-29 00:22:34 +0000

    change nginx config to allow more files opened

--- 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
+++ 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
@@ -1,5 +1,5 @@
 # Change files of nginxs to solve error failed (24: Too many open files)
-# take from https://www.claudiokuenzler.com/blog/850/nginx-socket-failed-24-too-many-open-files
+# take it from https://www.claudiokuenzler.com/blog/850/nginx-socket-failed-24-too-many-open-files
 
 # change limit file /etc/default/nginx
 file_line { 'IncreaseLimit':
@@ -7,25 +7,7 @@ line => 'ULIMIT="-n 4096"',
 path => '/etc/default/nginx'
 }
 
-# change /etc/security/limits.conf
-
-file_line {'limits':
-  line => 'nginx  soft nofile 30000',
-  path => '/etc/security/limits.conf'
-}
-
-file_line {'limits':
-  line => 'nginx  hard nofile 50000',
-  path => '/etc/security/limits.conf'
-}
-
-# how many files can be opened
-file_line {'HowManyFiles':
-line => 'worker_rlimit_nofile 30000;',
-path => '/etc/nginx/nginx.conf'
-}
-
-exec {'restart':
-path    => '/usr/bin/:/usr/sbin:/bin',
-command => 'sudo service nginx restart'
+exec { 'restart':
+command => 'sudo service nginx restart',
+path    => ['/usr/bin/','/usr/sbin','/bin']
 }
